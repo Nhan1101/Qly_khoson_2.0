cd3473ef5e5370cc652889a8e85b8d86a7884798
Tran Thi Thuy Tram <thuytram310705@gmail.com>
2026-04-14 23:05:29 +0700

    Đồng bộ giao diện và nút X cho các màn hình Tạo, Sửa, Xem chi tiết

--- Qly_khoson/nhapkho/urls.py
+++ Qly_khoson/nhapkho/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
 from .views import (
-    NhapKhoListView, TaoPhieuNhapView, SuaPhieuNhapView, XemPhieuView,
-    DsDonDatHangNccView, ChiTietDonDatHangView, TaoDonDatHangView, DanhSachNccView
+    NhapKhoHomeView, NhapKhoListView, TaoPhieuNhapView, SuaPhieuNhapView, XemPhieuView,
+    DsDonDatHangNccView, ChiTietDonDatHangView, TaoDonDatHangView, SuaDonDatHangView, DanhSachNccView
 )
 
 urlpatterns = [
-    # Nhập kho
-    path('nhap-kho/', NhapKhoListView.as_view(), name='nhap_kho'),
+    # Màn hình chọn nghiệp vụ nhập kho
+    path('nhap-kho-home/', NhapKhoHomeView.as_view(), name='nhap_kho_home'),
+    
+    # Nhập kho (Phiếu nhập)
+    path('nhap-kho/danh-sach/', NhapKhoListView.as_view(), name='nhap_kho'),
     path('nhap-kho/tao/', TaoPhieuNhapView.as_view(), name='tao_phieu_nhap'),
     path('nhap-kho/sua/', SuaPhieuNhapView.as_view(), name='sua_phieu_nhap'),
     path('nhap-kho/xem/', XemPhieuView.as_view(), name='xem_phieu'),
@@ -14,6 +17,7 @@ urlpatterns = [
     # Đơn đặt hàng
     path('don-dat-hang-ncc/', DsDonDatHangNccView.as_view(), name='ds_don_dat_hang_ncc'),
     path('don-dat-hang-ncc/tao/', TaoDonDatHangView.as_view(), name='tao_don_dat_hang'),
+    path('don-dat-hang-ncc/sua/', SuaDonDatHangView.as_view(), name='sua_don_dat_hang'),
     path('don-dat-hang-ncc/chi-tiet/', ChiTietDonDatHangView.as_view(), name='chi_tiet_don_dat_hang'),
     path('don-dat-hang-ncc/nha-cung-cap/', DanhSachNccView.as_view(), name='danh_sach_ncc'),
 ]
--- Qly_khoson/nhapkho/views.py
+++ Qly_khoson/nhapkho/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from suppliers.models import NhaCungCap
+
+# Màn hình chọn thao tác Nhập kho (Thay vì hiện danh sách ngay)
+class NhapKhoHomeView(TemplateView):
+    template_name = 'nhapkho/nhap_kho_home.html'
 
 # Nhập kho
 class NhapKhoListView(TemplateView):
@@ -7,9 +12,19 @@ class NhapKhoListView(TemplateView):
 
 class TaoPhieuNhapView(TemplateView):
     template_name = 'nhapkho/taophieunhap.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['nhacungcaps'] = NhaCungCap.objects.all()
+        return context
 
 class SuaPhieuNhapView(TemplateView):
     template_name = 'nhapkho/suaphieunhap.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['nhacungcaps'] = NhaCungCap.objects.all()
+        return context
 
 class XemPhieuView(TemplateView):
     template_name = 'nhapkho/xemctphieu.html'
@@ -23,6 +38,19 @@ class ChiTietDonDatHangView(TemplateView):
 
 class TaoDonDatHangView(TemplateView):
     template_name = 'nhapkho/taodondathang.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['nhacungcaps'] = NhaCungCap.objects.all()
+        return context
+
+class SuaDonDatHangView(TemplateView):
+    template_name = 'nhapkho/suadondathang.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['nhacungcaps'] = NhaCungCap.objects.all()
+        return context
 
 class DanhSachNccView(TemplateView):
     template_name = 'nhapkho/dsncc.html'
